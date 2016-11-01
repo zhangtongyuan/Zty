20070fcbc987e923e301c4c7f94291d38acfff14
zhangtongyuan <1140038681@qq.com>
2016-11-01 08:56:50 +0800

    lll

---
+++ app/src/main/java/com/example/zty/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.zty.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView= (TextView) findViewById(R.id.dj);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"点击了条目",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
